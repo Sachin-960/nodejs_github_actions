1a8f204ff0b108b65df4dcefdaf4707a9ec0dea1
Sachin-960 <dayalsachin0@gmail.com>
2025-05-23 14:49:51 +0530

    v1

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from GitHub Actions!\n');
+  res.end('Hello from Sachin Dayal!. This is a fully automated deployment using github actions, Terraform, Ansible. For more info visit: https://github.com/Sachin-960/nodejs_github_actions\n');
 });
 
 server.listen(80, '0.0.0.0', () => {
